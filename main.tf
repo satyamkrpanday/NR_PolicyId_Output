30208c3f1928560aacb0a2bbaf03b04701d4c37c
Satyam Kumar Pandey <106465505+satyamkrpanday@users.noreply.github.com>
2024-05-13 16:45:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,19 @@ resource "newrelic_alert_policy" "policy" {
 
   name = each.value
 }
+
+# Configure terraform
+terraform {
+  required_providers {
+    newrelic = {
+      source  = "newrelic/newrelic"
+    }
+  }
+}
+ 
+# Configure the New Relic provider
+provider "newrelic" {
+  account_id = var.NEW_RELIC_ACCOUNT_ID
+  api_key = var.NEW_RELIC_API_KEY   # Usually prefixed with 'NRAK'
+  region = "US"                    # Valid regions are US and EU
+}
